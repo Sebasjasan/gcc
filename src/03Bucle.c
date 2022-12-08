80a0705fa59403301ed6037c5014e3fe0a6a0aa5
Sebasjasan <118089634+Sebasjasan@users.noreply.github.com>
2022-12-08 16:36:01 -0500

    Bucle de 4

--- src/03Bucle.c
+++ src/03Bucle.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-int main ()
+void generarCuadrado(int signoAlterno)
 {
-    int numeroSigno=0;
-
-    printf("Ingrese el numero de signos: ");
-    scanf("%i", &numeroSigno);
-
-    for ( int i = 0; i < numeroSigno; i++)
+   for ( int i = 0; i < signoAlterno; i++)
     {
-        for ( int j = 0; j < numeroSigno; j++) 
-        if ((i==0) || (j==0) || (i==numeroSigno-1) || (j==numeroSigno-1))
+        for ( int j = 0; j < signoAlterno; j++) 
+        if ((i==0) || (j==0) || (i==signoAlterno-1) || (j==signoAlterno-1))
             if ((i+j) %2==0)
                 printf("+ ");
             else 
@@ -19,5 +14,27 @@ int main ()
             printf("  ");
         printf("\n\n");
     }   
+}
+
+int main ()
+{
+    int numeroSigno=0;
+
+    printf("Ingrese el numero de signos: ");
+    scanf("%i", &numeroSigno);
+
+    //generarCuadrado(numeroSigno);
+    for (int i = 0; i < numeroSigno; i++)
+    {   
+        if (i%2==0)
+        printf("+ ");
+        else
+        printf("- ");   
+        if (i%2==0)
+        printf("* "); 
+        else
+        printf("& ");    
+    }
+    
     return 0;
 }
--- src/04Escalera.c
+++ src/04Escalera.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main (void)
+
+{
+    int i, j, n;
+    printf("Escribe el numero de escalones: ");
+    scanf("%i",&n);
+
+    for (int a = 1; a < n; a++)
+    {
+        for (int b = 1; b < a; b++)
+            printf("*"," ");
+    }
+    printf("");
+    return 0;
+}
